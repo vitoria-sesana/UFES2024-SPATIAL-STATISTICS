d26e4cefbbc424f642c07f7458788be6505776df
vitoria-sesana <vitoriasesana11@gmail.com>
2024-06-20 08:46:07 -0300

    mapas qnt de gestantes adolescentes percentual e absoluto

--- script/S2_analise_inicial.R
+++ script/S2_analise_inicial.R
@@ -11,4 +11,23 @@ source("script/S1_tratamento.R")
 
 
 # variável resposta -------------------------------------------------------
-%>% 
+
+# percentual
+ggplot() +
+  geom_sf(data = data_gravidez_tratado, aes(fill = percent), color=NA) +
+  labs(title = "") +
+  scale_fill_distiller(palette = "Greens", direction = 1, 
+                       name='Percentual de\n Adolescentes Gestantes', 
+                       labels = scales::percent) +
+  scale_x_continuous(limits = c(-42, -39)) +
+  theme_void()
+
+# absoluto
+ggplot() +
+  geom_sf(data = data_gravidez_tratado, aes(fill = numeros_absolutos), color=NA) +
+  labs(title = "") +
+  scale_fill_distiller(palette = "Greens", direction = 1, 
+                       name='Percentual de\n Adolescentes Gestantes', 
+                       labels = scales::percent) +
+  scale_x_continuous(limits = c(-42, -39)) +
+  theme_void()
